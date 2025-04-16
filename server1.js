3ac2f866b8d23fb655a27383dbe5618847ca6a1d
s7237230 <s7237230@gmail.com>
2025-04-16 11:58:11 +0300

    Update server1.js

--- server1.js
+++ server1.js
@@ -1,8 +1,15 @@
-
 const express = require('express');
 const axios = require('axios');
 const app = express();
 app.use(express.json());
+
+// ✅ הוספת CORS לאפשר גישה מבחוץ (כולל GPT)
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 app.post('/forward', async (req, res) => {
   try {
